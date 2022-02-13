041e1ebb673a88f834adac838a96b41e8d59adc2
Conor Barthel <conor.barthel@gmail.com>
2022-02-13 07:41:32 -0700

    Fix: fix database problems so heroku works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_193308) do
+ActiveRecord::Schema.define(version: 2022_02_11_234018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,15 @@ ActiveRecord::Schema.define(version: 2022_02_12_193308) do
     t.string "description"
     t.string "pet_names"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pet_applications", force: :cascade do |t|
     t.bigint "pet_id"
     t.bigint "application_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-Pet.destroy.all
-Shelter.destroy.all
-Application.destroy.all
-
-@application = Application.create!(name: 'Mike',
-                                  street_address: "123 Blake st.",
-                                  city: "Denver",
-                                  state: "CO",
-                                  zipcode: "80211",
-                                  description: 'I work from home',
-                                  pet_names: 'Rufus',
-                                  status:'In Progress')
-@spca = Shelter.create!(name: "SPCA", rank:2, city: "Santa Cruz")
-@sadie = @spca.pets.create!(name: "Sadie", age:2)
-@maggie = @spca.pets.create!(name: "Maggie", age:1)
-@tucker = @spca.pets.create!(name: "Tucker", age:1)
+# Pet.destroy.all
+# Shelter.destroy.all
+# Application.destroy.all
+#
+# @application = Application.create!(name: 'Mike',
+#                                   street_address: "123 Blake st.",
+#                                   city: "Denver",
+#                                   state: "CO",
+#                                   zipcode: "80211",
+#                                   description: 'I work from home',
+#                                   status:'In Progress')
+# @spca = Shelter.create!(name: "SPCA", rank:2, city: "Santa Cruz")
+# @sadie = @spca.pets.create!(name: "Sadie", age:2)
+# @maggie = @spca.pets.create!(name: "Maggie", age:1)
+# @tucker = @spca.pets.create!(name: "Tucker", age:1)
